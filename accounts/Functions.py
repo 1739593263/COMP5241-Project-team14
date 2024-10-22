39c5d4f91f0f70f2eb984b5212fc3ada5515adaf
Du-Fang-z <24066918g@connect.polyu.hk>
2024-10-22 13:30:29 +0000

    basic code for account login and register

---
+++ accounts/Functions.py
@@ -0,0 +1,102 @@
+import sqlite3
+import os
+
+#返回数据库的游标cursor
+def connect_DB(name_db):
+    # connect to the database(if there isn't a database, it will be created)
+    conn = sqlite3.connect(f'{name_db}')
+    return conn
+
+def create_accounts_table(conn, db_name, table_name):
+    # 创建一个游标对象
+    cursor = conn.cursor()
+    # 创建一个表
+    cursor.execute(f'''
+    CREATE TABLE IF NOT EXISTS {table_name} (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT NOT NULL,
+        telephone INTEGER,
+        email TEXT NOT NULL,
+        password TEXT NOT NULL,
+        country TEXT,
+        type TEXT NOT NULL
+    )
+    ''')
+    # 提交事务
+    conn.commit()
+
+
+#是否存在表，存在返回1，不存在返回0
+def search_accounts_table(conn,db_name,table_name):
+    # 创建一个游标对象
+    cursor = conn.cursor()
+    # 查询 sqlite_master 表以检查是否存在名为 'table_name' 的表
+    cursor.execute(f"SELECT name FROM sqlite_master WHERE type='table' AND name='{table_name}';")
+
+    # 获取查询结果
+    result = cursor.fetchone()
+
+    if result:
+        print("表存在。")
+        return 1
+    else:
+        print("表不存在。")
+        return 0
+
+#检查是否存在数据库和table, 准备成功返回1，准备失败返回0
+def check_database_and_table():
+    #连接到数据库
+    conn = connect_DB("CV.db")
+    if search_accounts_table(conn,"CV.db","ACCOUNTS") == 0:
+        create_accounts_table(conn, "CV.db", "ACCOUNTS")
+        print("NO TABLE, THE TABLE WILL BE CREATED")
+    elif search_accounts_table(conn,"CV.db","ACCOUNTS") == 1:
+        print("SELECT THE TABLE SUCCESSFULLY")
+    else:
+        print("UNEXPECT ERROR")
+        return 0
+    print("PREPARING IS COMPLETE")
+    #关闭数据库
+    conn.close()
+    return 1
+
+
+
+
+#插入数据用户信息数据，如果存在则返回0，如果不存在则返回1
+def insert_account(conn,table_name,account_dict):
+    # 创建一个游标对象
+    cursor = conn.cursor()
+    
+    # 检查用户是否存在
+    cursor.execute(f"SELECT * FROM {table_name} WHERE email = '{account_dict["email"]}'")
+    result = cursor.fetchone()
+
+    if result:
+        # 用户已存在，返回 0
+        return 0
+    else:
+        # 用户不存在，插入新用户
+        sql=f"""INSERT INTO {table_name} VALUE ({account_dict["id"]},{account_dict["name"]},
+            {account_dict["telephone"]},{account_dict["email"]},{account_dict["password"]},
+            {account_dict["country"]},{account_dict["type"]}) """
+        cursor.execute(sql)
+        conn.commit()
+        return 1
+
+#查询登陆数据数据，正确返回1，不正确返回0
+def login_account(conn,table_name,email,password):
+    #获取游标
+    cursor = conn.cursor()
+
+    # 检查用户是否存在
+    cursor.execute(f"SELECT * FROM {table_name} WHERE email = '{email}' AND password = '{password}'")
+    result = cursor.fetchone()
+
+    if result:
+        return 1
+    else:
+        return 0
+
+
+
---
+++ accounts/main.py
@@ -0,0 +1,95 @@
+#database name is CV.db, Table name is ACCOUNTS
+#changing the login successfully code, let it turn to the successfully page
+import streamlit as st
+import Functions as fc
+
+# 登录页面
+def login_page():
+    #数据库准备
+    prepare_state = fc.check_database_and_table()
+    if prepare_state == 0:
+        st.warning("Sorry, there is something wrong with the app, please try it again")
+    else:
+        st.title("Login")
+    
+        email = st.text_input("Please input your email")
+        password = st.text_input("Please input your password", type="password")
+    
+        if st.button("Login"):
+            if email == "" or password == "":
+                st.warning("please input your email or password")
+            else:
+                conn = fc.connect_DB("CV.db")
+                login_result = fc.login_account(conn, "ACCOUNTS", email, password)
+                if login_result == 1:
+                    st.success("Login successfully!")
+                    # turning to the app page
+                elif login_result == 0:
+                    st.warning("Email or Password is wrong!")
+                else:
+                    st.warning("Sorry for the unexpect error, please try it again.")
+                conn.close()
+        if st.button("Register"):
+            # turn to the register page
+            st.session_state.page = "register"
+
+def register_page():
+    st.title("Register")
+    
+    st.write("Name:")
+    name = st.text_input("Example: Zack", key = "name_input")
+    st.write("Phone:")
+    phone = st.text_input("Example: 12345678", key = "phone_input")
+    st.write("Email:")
+    email = st.text_input("Example:12345678@163.com", key = "email_input")
+    st.write("Password:")
+    password = st.text_input("",type="password", key = "password_input")
+    st.write("Confirm-password:")
+    confirm_password = st.text_input("", type="password", key = "password_confirm")
+    st.write("country:")
+    country = st.selectbox("Country", ["Chinese", "America", "England", "Other country"])
+    st.write("Account type:")
+    user_type = st.selectbox("Type", ["Employee", "Employer"])
+    
+    if st.button("Register"):
+        if name == "" or len(name) > 16:
+            st.warning("Name cannot be empty or more than 16 Letters.")
+        elif email == "":
+            st.warning("Email cannot be empty.")
+        elif len(password) < 8 or len(password) > 16:
+            st.warning("Password cannot be less than 8 digits and more than 16 digits.")
+        elif password != confirm_password:
+            st.warning("Confirm-password should match the password.")
+        elif user_type == "":
+            st.warning("Type cannot be empty!")
+        else:
+            # 将数据存入数据库
+            conn = fc.connect_DB("CV.db")
+            account_register = {"name":name, "telephone":phone, "email": email, "password":password, "country":country, "type":user_type}
+            
+            register_result = fc.insert_account(conn,"ACCOUNTS", account_register)
+            if register_result == 0:
+                st.warning("The account is exist")
+            elif register_result == 1:
+                st.success("Register successfully")
+            else:
+                st.warning("Sorry for the unexpect error, please try it again.")
+            conn.close()
+    
+    if  st.button("Login"):
+        st.session_state.page = "login"  # 注册成功后返回登录页面
+
+
+# 主程序
+def main():
+    if 'page' not in st.session_state:
+        st.session_state.page = "login"  # 默认显示登录页面
+
+    if st.session_state.page == "login":
+        login_page()
+    elif st.session_state.page == "register":
+        register_page()
+
+if __name__ == "__main__":
+    main()
+
